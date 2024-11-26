388ca3342db7b685c3bdfe0564c4caabf1bd273b
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-11-26 16:25:10 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace APILibraryDaltonismo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine(Request().GetAwaiter().GetResult());
+        }
+        public static async Task<string> Request()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5000");
+            HttpResponseMessage response = await client.GetAsync(
+                "GetScores");
+            string requestResponse = "";
+            if (response.IsSuccessStatusCode)
+            {
+                requestResponse = await response.Content.ReadAsStringAsync();
+            }
+            return requestResponse;
+        }
+    }
+}
